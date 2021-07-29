47fce8d05daa7c164970a55a81ba2a4bf6927e08
Gokul <gokul.26.12.1995@gmail.com>
2021-07-29 18:20:47 +0530

    task 7

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://restcountries.eu/rest/v2/all");
 xhr.onload = function () {
   var data = JSON.parse(this.response);
-  let a = data.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue.population
-  },0);
-  console.log(a);
+  function region(a) {
+    if (a.currencies[0].code =="USD") {
+      console.log(a.name);
+    }
+  }
+  data.filter(region);
 };
 xhr.send();
