55a54972a69032e5a38894313e1ee299ffb08711
Gokul <gokul.26.12.1995@gmail.com>
2021-07-29 12:04:22 +0530

    task 5

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let login = 'Employee';
-let message = (login == 'Employee') ? 'Welcome':
-  (login == 'Director') ? 'Greetings' :
-  (login == '') ? 'No login' :
-  '';
-console.log(message);
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+xhr.onload = function () {
+  var data = JSON.parse(this.response);
+  data.forEach(function(name){
+    console.log(name);
+  })
+};
+xhr.send();
