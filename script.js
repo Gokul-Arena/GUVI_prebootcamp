bac205a5c90b3452f45fb561c40b77b1a0e16530
Gokul <gokul.26.12.1995@gmail.com>
2021-07-29 18:18:47 +0530

    task 7

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-var numsArr = [[1, 2, 3, 4, 5],[ 6, 7, 8, 9, 10, 11]];
-var sum_odd=0;
-var sum_even=0;
-for (var i = 0; i < numsArr.length; i++) {
- var inner_array = numsArr[i];
- for(var j = 0 ; j < inner_array.length;j++ ){
- if(inner_array[j]%2 !== 0)
- {
- sum_odd += inner_array[j];
- }
- else
- {
- sum_even += inner_array[j]
- }
-}
-}
-console.log(sum_odd);
-console.log(sum_even);
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+xhr.onload = function () {
+  var data = JSON.parse(this.response);
+  let a = data.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue.population
+  },0);
+  console.log(a);
+};
+xhr.send();
