28167698cd04d01960f39ad1850fa4a3807c61ce
Gokul <gokul.26.12.1995@gmail.com>
2021-07-31 23:58:34 +0530

    hi

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-var ar = [121, 200, 313, 13, 979, 10, 202, 11];
-var a =((arr) => {
-  var b =[];
-  var c;
-  for(i=0; i<arr.length; i++) {
-    var a = arr[i];
-    var str = a + '';
-    var c = true;
-  for (j=0; j<str.length/2; j++) {
-    if(str[j] !== str[str.length-1-j]) {
-       c = false;
-       break;
-    }
-  }
-  if (c == true) {
-    b.push(arr[i]);
-  }
+function company(firstName, lastName, No, Address, City){
+  console.log({
+      firstName,
+      lastName,
+      No,
+      Address,
+      City
+  })
 }
-console.log(b);
-})(ar);
+company('Guvi', 'Geek', '6', 'IIT-M RP','Chennai');
